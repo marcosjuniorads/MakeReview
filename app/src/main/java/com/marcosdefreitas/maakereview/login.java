4cc085d081ef45b38ba6751bc2336c2691a6bc29
Marcos <marcosjunior.ads@gmail.com>
2017-11-14 01:19:30 -0200

    Adjust layout login

---
+++ app/src/main/java/com/marcosdefreitas/maakereview/login.java
@@ -0,0 +1,16 @@
+package com.marcosdefreitas.maakereview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    //teste
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
