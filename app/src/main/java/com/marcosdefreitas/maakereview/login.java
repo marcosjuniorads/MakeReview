6a7e0ecdf92491e8e4aaacb4b875765524dd0f43
Marcos <marcosjunior.ads@gmail.com>
2017-11-14 17:12:47 -0200

    Adjust layout login

--- app/src/main/java/com/marcosdefreitas/maakereview/login.java
+++ app/src/main/java/com/marcosdefreitas/maakereview/login.java
@@ -2,15 +2,46 @@ package com.marcosdefreitas.maakereview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+public class login extends AppCompatActivity implements View.OnClickListener {
 
-    //teste
+    Button btnEntrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_login);
+
+        btnEntrar = (Button)findViewById(R.id.btn_entrar_login);
+        btnEntrar.setOnClickListener(login.this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Mudando..", Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(login.this, relativeLayout.class));
+            }
+        });
     }
 
 }
